Fixes to the perplexity formula and introducing documentation

perplexity.py
@@ -1,3 +1,7 @@
+'''This script was written by Maria Szawerna for the LT2222 V22 Machine Learning course at the University of Gothenburg between 16 and 21.03.2022
+    It is intended to be run from the command line together with 2 command line arguments: the input file that the model and other data are obtained from,
+    the second input file containing the testing data; nevertheless, the functions and some variables can be imported into other scripts on their own.'''
+
 import argparse
 import pickle
 from sklearn.svm import SVC
@@ -14,15 +18,18 @@ parser.add_argument("test_file", help="the file you want to take the testing dat
 
 args = parser.parse_args()
 
+# this performs the same function here as in sample.py, but for the sake of clarity I did not import it but just copied it
 consonants_set = set(['b', 'c', 'd', 'f', 'g', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 's', 't', 'v', 'x', 'y', 'z', 'h', 'r', 't', 'w', 'y'])
 
 def import_model(filename):
+    # this function is identical to the one in test.py but copied and not imported for the sake of simplicity
     with open(filename, 'rb') as model_file:
-        model, possible_columns = pickle.load(model_file)
+        model, possible_columns, nr_of_samples = pickle.load(model_file)
 
-    return model, possible_columns
+    return model, possible_columns, nr_of_samples
 
 def sample_lines(filename):
+    # this function is identical to the one in sample.py but copied and not imported for the sake of simplicity
     if filename.endswith(".gz"):
         unzipped_file = gzip.open(filename, "rb")
         lines = unzipped_file.readlines()
@@ -48,6 +55,7 @@ def sample_lines(filename):
         quit()
 
 def find_closest_consonant(line):
+    # this function is identical to the one in sample.py but copied and not imported for the sake of simplicity
     for character in line:
         if character in consonants_set:
             return character
@@ -56,6 +64,7 @@ def find_closest_consonant(line):
     return False
 
 def create_samples(sampled_lines):
+    # this function is identical to the one in sample.py but copied and not imported for the sake of simplicity
     all_samples = []
     for line in sampled_lines:
         for i in range(0, len(line)-5):
@@ -67,6 +76,7 @@ def create_samples(sampled_lines):
     return all_samples  
 
 def create_vectors(all_samples, possible_columns):
+    # this function is identical to the one in train.py but copied and not imported for the sake of simplicity
     random.shuffle(all_samples) 
     sample_vectors = []
     for sample in all_samples:
@@ -85,27 +95,37 @@ def create_vectors(all_samples, possible_columns):
     return sample_vectors
 
 def create_df(sample_vectors):
-
+    # this function is identical to the one in train.py but copied and not imported for the sake of simplicity
     vector_array = np.array(sample_vectors)
     vector_df = pd.DataFrame(vector_array)
 
     return vector_df
 
 def split_samples(fulldf):
+    # this function is identical to the one in train.py but copied and not imported for the sake of simplicity
     column_number = len(fulldf.columns)
     test_X = fulldf.iloc[:, 0:column_number-2]
     test_y = fulldf.iloc[:, column_number-1]
 
     return test_X, test_y
 
-def return_probabilities(model, sample_vectors):
-    probabilities = model.predict_log_proba(sample_vectors)
-    return probabilities
-
-### experimenting
-def retrieve_correct_probs(model, test_X, test_y):
-    correct_probs = []
+def return_probabilities(model, test_X):
+    # this function takes the model and a DataFrame of vectors without the classes and returns the probabilities predicted by the model; since
+    # sklearn uses by default the np.log() function to return log probabilities, these need to be converted from base e to base 2 before being
+    # used in the perplexity formula; this is also done within this function and an array of log2 probabilities is returned
     probabilities = model.predict_log_proba(test_X)
+    base_2_logs = np.divide(probabilities, np.log(2))
+
+    return base_2_logs
+
+def retrieve_correct_probs(model, test_X, test_y, nr_of_samples):
+    # this function takes the trained model, test vectors, corresponding test classes, and the number of samples and first retrieves the log2 probabilities
+    # using the return_probabilities() function, then retrieves the corresponding classes contained within the model, and then based on that and the list of
+    # true classification retrieves the probability assigned by the model to the "correct" answer; in case none such probability exists, a simplified 
+    # version of add-1/Laplace smoothing is implemented (as suggested by Asad Sayeed) where the log2 of 1 divided by the number of training samples is assigned
+    # to it instead; a list of probabilities associated with the correct classification is returned
+    correct_probs = []
+    probabilities = return_probabilities(model, test_X)
     classes = model.classes_
     for i in range(0,len(test_y)):
         correct_ans = test_y[i]
@@ -114,43 +134,37 @@ def retrieve_correct_probs(model, test_X, test_y):
             if classes[j] == correct_ans:
                 correct_prob = probs[j]
                 correct_probs.append(correct_prob)
-            if correct_ans not in classes:  # if there is no probability for the correct answer according to the model
-                correct_prob = 1/len(test_y)  # accounting for 0 probabilities
-                correct_prob_log = math.log(correct_prob)
-                correct_probs.append(correct_prob_log)
+        if correct_ans not in classes:  # if there is no probability for the correct answer according to the model
+            correct_prob = 1/nr_of_samples  # accounting for 0 probabilities
+            correct_prob_log = math.log(correct_prob,2)  # base 2 logarithm
+            correct_probs.append(correct_prob_log)
 
     return correct_probs
 
-def perplexity(model, test_X, test_y):
-    correct_probs = retrieve_correct_probs(model, test_X, test_y)
+def perplexity(model, test_X, test_y, nr_of_samples):
+    # this function takes the trained model, the test vectors, corresponding test classes, and the number of samples, uses the retrieve_correct_probs() function to
+    # obtain a list of log2 probabilities associated with the correct answers, compares the length of that to the length of the true answers, prints 'inf' if they are
+    # uneven (which should not happen as smoothing is built in), and otherwise calculates the perplexity by raising the base 2 to the power of negative one over the
+    # number of test samples times the sum of the log2 probabilities predicted by the model;
+    correct_probs = retrieve_correct_probs(model, test_X, test_y, nr_of_samples)
     if len(correct_probs) != len(test_y):
-        return "inf"
+        print("inf")
     else:
         sample_number = len(test_X)
+        base = 2
         exponent = -(1/sample_number)*(sum(correct_probs))
-        pp = np.exp(exponent)  # because scikit has all the log probabilities in base e, the formula for that is from here https://stats.stackexchange.com/questions/10302/what-is-perplexity
-        return pp
+        pp = math.pow(base, exponent)
+        print(f'The perplexity of this {model} model is equal to {pp}')
 
 
 
 if __name__ == "__main__":
-    model, possible_columns = import_model(args.model_file)
+    model, possible_columns, nr_of_samples = import_model(args.model_file)
 
     sampled_lines = sample_lines(args.test_file)
     full_samples = create_samples(sampled_lines)
     sample_vectors = create_vectors(full_samples, possible_columns)
     data_frame = create_df(sample_vectors)
     test_X, test_y = split_samples(data_frame)
-    probabilities = return_probabilities(model, test_X)
-    print(model.classes_)
-    print(probabilities)
-    print(len(data_frame))
-    print(len(probabilities))
-    print(len(model.classes_))
-    print(len(probabilities[0]))
-    
-    correct_probs = retrieve_correct_probs(model, test_X, test_y)
-    print(len(correct_probs))
 
-    pp = perplexity(model, test_X, test_y)
-    print(pp)
+    perplexity(model, test_X, test_y, nr_of_samples)

test.py
@@ -1,3 +1,8 @@
+'''This script was written by Maria Szawerna for the LT2222 V22 Machine Learning course at the University of Gothenburg between 16 and 21.03.2022
+    It is intended to be run from the command line together with 3 command line arguments: the input file that the model and other data are obtained from,
+    the second input file containing the testing data, and the type of average that will be used for some of the measures; 
+    nevertheless, the functions and some variables can be imported into other scripts on their own.'''
+
 import argparse
 import pickle
 from sklearn.svm import SVC
@@ -15,18 +20,21 @@ parser.add_argument("average_type", help="the kind of average that you want impl
 args = parser.parse_args()
 
 def import_testing_data(filename):
+    # this function imports the list of all the testing samples obtained from sample.py and saved in a pickle file and returns them as a tuple
     with open(filename, 'rb') as testing_file:
         test_samples = pickle.load(testing_file)
 
     return test_samples
 
 def import_model(filename):
+    # this function imports the trained model, the list of possible columns obtained from train.py, and the number of training samples saved in a pickle file and returns them as a tuple
     with open(filename, 'rb') as model_file:
-        model, possible_columns = pickle.load(model_file)
+        model, possible_columns, nr_of_samples = pickle.load(model_file)
 
-    return model, possible_columns
+    return model, possible_columns, nr_of_samples
 
 def create_vectors(all_samples, possible_columns):
+    # this function is identical to the one in train.py but copied and not imported for the sake of simplicity
     random.shuffle(all_samples) 
     sample_vectors = []
     for sample in all_samples:
@@ -45,13 +53,14 @@ def create_vectors(all_samples, possible_columns):
     return sample_vectors
 
 def create_df(sample_vectors):
-
+    # this function is identical to the one in train.py but copied and not imported for the sake of simplicity
     vector_array = np.array(sample_vectors)
     vector_df = pd.DataFrame(vector_array)
 
     return vector_df
 
 def split_samples(fulldf):
+    # this function is identical to the one in train.py but copied and not imported for the sake of simplicity
     column_number = len(fulldf.columns)
     test_X = fulldf.iloc[:, 0:column_number-2]
     test_y = fulldf.iloc[:, column_number-1]
@@ -59,6 +68,8 @@ def split_samples(fulldf):
     return test_X, test_y
 
 def eval_model(model, test_X, test_y, average_type):
+    # rhis function evaluates what kind of average is requested, and if it is one of the permitted types it evaluates the model on the testing data and prints to the terminal
+    # the sklearn metrics accuracy, precision, recall, and f1 score, the latter three averaged using the requested average type
     if average_type == 'micro' or average_type == 'macro':
         pred_y = model.predict(test_X)
         accuracy = skm.accuracy_score(test_y, pred_y)
@@ -72,11 +83,11 @@ def eval_model(model, test_X, test_y, average_type):
     print(f"For the model {model} the following scores were obtained with {average_type}-averaging (wherever applicable): \n\taccuracy = {accuracy} \n\tprecision = {precision} \n\trecall = {recall} \n\tf1 score = {f1}")
 
 if __name__ == "__main__":
-    model, possible_columns = import_model(args.model_file)
+    # here the functions above are used to first retrieve the trained model and the data packed together with it, then retrieve the testing data and process it in a similar fashion, and finally to evaluate the model;
+    # all of the necessary arguments are obtained from the command line using argparse
+    model, possible_columns, nr_of_samples = import_model(args.model_file)
     test_samples = import_testing_data(args.test_file)
     test_vectors = create_vectors(test_samples, possible_columns)
     data_frame = create_df(test_vectors)
     test_X, test_y = split_samples(data_frame)
     eval_model(model, test_X, test_y, args.average_type)
-
-    print(len(possible_columns))

train.py
@@ -1,3 +1,8 @@
+'''This script was written by Maria Szawerna for the LT2222 V22 Machine Learning course at the University of Gothenburg between 16 and 21.03.2022
+    It is intended to be run from the command line together with 3 command line arguments: the input file that the training data is obtained from,
+    the output file that the trained model, size od the training data, and the column names will be saved to, and the name of the model that will
+    be trained, SVC or NB; nevertheless, the functions and some variables can be imported into other scripts on their own.'''
+
 import argparse
 import pickle
 from sklearn.svm import SVC
@@ -13,15 +18,20 @@ parser.add_argument("model", help="the model you want to use: choose between SVC
 
 args = parser.parse_args()
 
+# this performs the same function here as in sample.py (and perplexity.py), but for the sake of clarity I did not import it but just copied it
 consonants_set = set(['b', 'c', 'd', 'f', 'g', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 's', 't', 'v', 'x', 'y', 'z', 'h', 'r', 't', 'w', 'y'])
 
 def import_training_data(filename):
+    # this function imports the list of all the training samples and the list of possible columns obtained from sample.py and saved in a pickle file and returns them as a tuple
     with open(filename, 'rb') as training_file:
         all_samples, possible_columns = pickle.load(training_file)
 
     return all_samples, possible_columns
 
 def create_vectors(all_samples, possible_columns):
+    # this function takes a list of samples and a list of possible columns to judge them by and creates a vector for each sample, returning a list of those vectors; 
+    # this is done by iterating over all the column names and checking if those exist in the sample and appending a 0 or 1, and finally appending the consonant
+    # the samples are also shuffled before this is done in case that would introduce any bias in the machine
     random.shuffle(all_samples) 
     sample_vectors = []
     for sample in all_samples:
@@ -40,13 +50,15 @@ def create_vectors(all_samples, possible_columns):
     return sample_vectors
 
 def create_df(sample_vectors):
-
+    # this function takes the list of vectors corresponding to the samples and first turns it into a numpy array which then is converted into a pandas DataFrame, 
+    # which is subsequently returned
     vector_array = np.array(sample_vectors)
     vector_df = pd.DataFrame(vector_array)
 
     return vector_df
 
 def split_samples(fulldf):
+    # this function splits the dataframe into the X and y, meaning the vectors and the corresponding classes, those two DataFrames are returned
     column_number = len(fulldf.columns)
     train_X = fulldf.iloc[:, 0:column_number-2]
     train_y = fulldf.iloc[:, column_number-1]
@@ -54,6 +66,8 @@ def split_samples(fulldf):
     return train_X, train_y
 
 def train(train_X, train_y, model):
+    # this function determines which kind of model will be trained and quits the script in case a request is made that cannot be fulfilled; if the selected model is
+    # available, it is trained on the given train_X and train_y DataFrames, and a trained model is returned
     if model == "SVC":
         machine = SVC(kernel='linear', probability=True)
     elif model == "NB":
@@ -65,14 +79,19 @@ def train(train_X, train_y, model):
     
     return machine
 
-def save_model(model, possible_columns, filename):
+def save_model(model, possible_columns, nr_of_samples, filename):
+    # this function saves the trained model along with the names of the columns it was trained on and the number of the samples it was trained on into a given file
     with open(filename, 'wb') as model_file:
-        pickle.dump((model, possible_columns), model_file)
+        pickle.dump((model, possible_columns, nr_of_samples), model_file)
 
 if __name__ == "__main__":
+    # here the functions above are used to first obtain the data from the file, then count the samples, create the vectors, create the DataFrame, split it, train the machine, and save it;
+    # all of the necessary arguments are obtained from the command line using argparse
     all_samples, possible_columns = import_training_data(args.input_file)
+    nr_of_samples = len(all_samples)
     sample_vectors = create_vectors(all_samples, possible_columns)
     data_frame = create_df(sample_vectors)
     train_X, train_y = split_samples(data_frame)
     machine = train(train_X, train_y, args.model)
-    save_model(machine, possible_columns, args.output_file)
+    save_model(machine, possible_columns, nr_of_samples, args.output_file)
+    print("Training finished successfully.")